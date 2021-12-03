7c6c82786d13f1d5023a6edc7ba6e49e0ea8383d
liup <mail_bob@163.com>
2021-12-04 00:39:42 +0800

    [+] 添加了归并排序

--- src/com/bobsystem/algorithms/sort/SortAlgorithms.java
+++ src/com/bobsystem/algorithms/sort/SortAlgorithms.java
@@ -1,22 +1,23 @@
 package com.bobsystem.algorithms.sort;
 
+/**
+ * 排序方法有：
+ *   插入排序(直接插入排序、希尔排序、二分法插入排序、二路插入排序、共享栈插入排序)
+ *   交换排序(冒泡排序、快速排序)
+ *   选择排序(直接选择排序、树型选择排序、堆排序)
+ *   合并排序
+ */
 public class SortAlgorithms {
 
-    // 排序方法有：
-    //  插入排序(直接插入排序、希尔排序、二分法插入排序、二路插入排序、共享栈插入排序)
-    //  交换排序(冒泡排序、快速排序)
-    //  选择排序(直接选择排序、树型选择排序、堆排序)
-    //  合并排序
-
+    //region insert sort 插入排序、希尔排序
     /**
-     * 插入排序法逻辑：每取一个数把它抽出，把前面大于它的数往后挪一位
-     * 5 4 3 2 1
-     * 4 5 3 2 1
-     * 3 4 5 2 1
-     * 2 3 4 5 1
-     * 1 2 3 4 5
+     * 插入排序法。
+     *   逻辑：从第二个元素向后取数，每取一个数，把大于它的数往后挪一位
+     *
+     * 时间复杂度接近 o(n^2)
      */
     public void insertSort(int[] arr) {
+        // { 7, 8, 9, 6, 5, 4, 2, 3, 1, 0 }
         for (int i = 1, c = arr.length; i < c; ++i) {
             int tmp = arr[i];
             int j = i;
@@ -24,34 +25,62 @@ public class SortAlgorithms {
                 if (arr[j - 1] > tmp) {
                     arr[j] = arr[j - 1];
                 }
-                else {
-                    break;
-                }
+                else break;
             }
             arr[j] = tmp;
         }
     }
 
     /**
-     * 选择排序法逻辑：第1位数依次向后面的数比较
+     * 希尔排序。希尔排序质疑了冒泡排序中相邻元素比较和交换的必要性。
+     *   逻辑：将数组中一部分元素依次与另一部分比较，具体实现是定义一个间隔，并缩小间隔。
+     *
+     * 时间复杂度接近 o(n * 2)
+     */
+    public void shellSort(int[] arr) {
+        int times = 0;
+        int length = arr.length;
+        int gap = length;
+        while ((gap /= 2) > 0) {
+            for (int i = gap; i < length; ++i) {
+                int tmp = arr[i];
+                for (; i - gap >= 0 && tmp < arr[i - gap]; i -= gap) {
+                    // 这个循环的意义是，将当前元素与之前每个固定间隔位置的元素比较。
+                    arr[i] = arr[i - gap];
+                    times += 1;
+                }
+                arr[i] = tmp;
+            }
+        }
+        System.out.println("希尔排序一共交换的次数：" + times);
+    }
+    //endregion insert sort 插入排序、希尔排序
+
+    //region select sort
+    /**
+     * 选择排序法逻辑：第 1 位数依次向后面的数比较
      * 5 4 3 2 1
      * 4 5 3 2 1
      * 3 5 4 2 1
      * 2 5 4 3 1
      * 1 5 4 3 2
+     *
+     * 时间复杂度接近 o(n ^ 2)
      */
     public void selectSort(int[] arr) {
-        for (int i = 0, c = arr.length; i < c; ++i) {
-            for (int j = i + 1; j < c; ++j) {
-                if (arr[i] > arr[j]) {
-                    int tmp = arr[i];
+        for (int i = 0, m = arr.length - 1; i < m; ++i) {
+            for (int j = i + 1, n = arr.length; j < n; ++j) {
+                int tmp = arr[i];
+                if (tmp > arr[j]) {
                     arr[i] = arr[j];
                     arr[j] = tmp;
                 }
             }
         }
     }
+    //endregion select sort
 
+    //region swap sort 冒泡排序、快速排序
     /**
      * 冒泡排序法逻辑：相邻的两个数比较
      * 5 4 3 2 1
@@ -59,10 +88,13 @@ public class SortAlgorithms {
      * 4 3 5 2 1
      * 4 3 2 5 1
      * 4 3 2 1 5
+     *
+     * 时间复杂度接近 o(n^2)
      */
     public void bubbleSort(int[] arr) {
-        for (int i = 0, c = arr.length; i < c; ++i) {
-            for (int j = 0; j < c - 1 - i; ++j) { // 再减i的目的是，相邻两两比较，保证最后i位已经正序排列
+        for (int i = 0, c = arr.length; i < c - 1; ++i) {
+            // 再减 i 的目的是，相邻两两比较，保证最后 i 位已经正序排列
+            for (int j = 0; j < c - 1 - i; ++j) {
                 if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
                     arr[j] = arr[j + 1];
@@ -74,17 +106,18 @@ public class SortAlgorithms {
 
     /**
      * 快速排序法逻辑：将数组视为两部分，让一部分都比另一部分小
-     * 具体逻辑：右侧数值依次和 pivot(轴值)比较，如果小于，将右侧数值赋给左侧
-     *          左侧数值依次和 pivot比较，如果大于，将左侧数值赋给右侧 变化过的right位置
+     * 具体逻辑：右侧数值依次和 pivot(轴值) 比较，如果小于，将右侧数值赋给左侧
+     *          左侧数值依次和 pivot 比较，如果大于，将左侧数值赋给右侧 变化过的 right 位置
      *          然后将 pivot 赋予 变化过的left位置
      */
-    private void quickSort(int arr[], int low, int high) {
+    public void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
 
+    private void quickSort(int[] arr, int low, int high) {
         int left = low;  // left、right 是下标
         int right = high;
-
         int pivot = arr[low]; // pivot 取第1位数
-
         while (left < right) {
             while (left < right && arr[right] >= pivot) {
                 right--;
@@ -107,12 +140,53 @@ public class SortAlgorithms {
             quickSort(arr, left + 1, high);
         }
     }
+    //endregion swap sort
 
-    public void quickSort(int arr[]) {
-        quickSort(arr, 0, arr.length - 1);
+    //region merge sort
+    /**
+     * 归并排序。逻辑：
+     *   递归将数组对半分割，每次得到一对子数组，直到每个子数组只有一个元素。
+     *   再将这对子数组的右侧 与 左侧对应位置比较。
+     */
+    public void mergeSort(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
     }
 
-    // reverse array
+    private void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) return;
+        int middle = (high - low) / 2 + low;
+        //
+        mergeSort(arr, low, middle);
+        mergeSort(arr, middle + 1, high);
+        ////
+        //System.out.printf("low, high = %d, %d; left, right = %s, %s %n",
+        //    low, high,
+        //    Arrays.toString(Arrays.copyOfRange(arr, low, middle + 1)),
+        //    Arrays.toString(Arrays.copyOfRange(arr, middle + 1, high + 1)));
+        int i = low;
+        for (int j = middle + 1; i <= middle && j <= high; ++i, ++j) {
+            int eleI = arr[i];
+            int eleJ = arr[j];
+            if (eleI > eleJ) {
+                arr[i] = arr[j];
+                arr[j] = eleI;
+            }
+        }
+        if (i <= middle) { // 对半分割的过程，左侧也许会比右侧多一个元素
+            for (int j = high; j > middle; --j) {
+                int eleI = arr[i];
+                if (eleI > arr[j]) {
+                    arr[i] = arr[j];
+                    arr[j] = eleI;
+                }
+            }
+        }
+    }
+    //endregion merge sort
+
+    /**
+     * reverse array
+     */
     public void reverseArray(int[] arr) {
         for (int i = 0, j = arr.length - 1, c = arr.length / 2; i < c; ++i, --j) {
             int tmp = arr[i];
--- test/com/bobsystem/algorithms/sort/SortAlgorithmsTest.java
+++ test/com/bobsystem/algorithms/sort/SortAlgorithmsTest.java
@@ -1,36 +1,37 @@
 package com.bobsystem.algorithms.sort;
 
+import org.junit.After;
 import org.junit.Test;
 
 import java.util.Arrays;
 
 public class SortAlgorithmsTest {
 
-    private final SortAlgorithms sortAlgorithms = new SortAlgorithms();
+    private final int[] arr = { 7, 8, 9, 6, 5, 4, 2, 3, 1, 0 };
 
+    private final SortAlgorithms algorithm = new SortAlgorithms();
+
+    //region insert sort 直接插入排序、希尔排序
     @Test
     public void testInsertSort() {
-        int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-        sortAlgorithms.insertSort(arr);
-
-        System.out.println(Arrays.toString(arr));
+        this.algorithm.insertSort(this.arr);
     }
+
+    @Test
+    public void testShellSort() {
+        this.algorithm.shellSort(this.arr);
+    }
+    //region insert sort 直接插入排序、希尔排序
 
     @Test
     public void testSelectSort() {
-        int[] arr = { -2, -5, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 12, 10, 11 };
-        sortAlgorithms.selectSort(arr);
-
-        System.out.println(Arrays.toString(arr));
+        algorithm.selectSort(arr);
     }
 
+    //region swap sort 冒泡排序、快速排序
     @Test
     public void testBubbleSort() {
-
-        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-        this.sortAlgorithms.bubbleSort(arr);
-
-        System.out.println(Arrays.toString(arr));
+        this.algorithm.bubbleSort(this.arr);
     }
 
     @Test
@@ -40,18 +41,26 @@ public class SortAlgorithmsTest {
         //int[] arr = { -10, -9, -8, -7, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         int[] arr = {5, 4, 3, 2, 2, 1};
 
-        this.sortAlgorithms.quickSort(arr);
+        this.algorithm.quickSort(arr);
 
         System.out.println(Arrays.toString(arr));
     }
+    //endregion swap sort 冒泡排序、快速排序
+
+    //region merge sort
+    @Test
+    public void testMergeSort() {
+        this.algorithm.mergeSort(this.arr);
+    }
+    //endregion merge sort
 
     @Test
     public void testReverseArray() {
+        this.algorithm.reverseArray(this.arr);
+    }
 
-        int[] arr = {0, 1, 2, 3, 4, 5, 6};
-
-        this.sortAlgorithms.reverseArray(arr);
-
-        System.out.println(Arrays.toString(arr));
+    @After
+    public void afterTest() {
+        System.out.println(Arrays.toString(this.arr));
     }
 }
